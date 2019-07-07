3c925389a85fe18bce3e70de4c7e77fc2400eb16
İdil Zeynep Alemdar <idilzeynepalemdar@gmail.com>
2019-07-07 17:50:21 +0300

    refactor: code modulerized into functions

--- apod.py
+++ apod.py
@@ -3,29 +3,58 @@
 import urllib3
 import urllib.request
 import json
-import os
-import subprocess as sp
+from subprocess import Popen
 
 directory = '/home/cydonia/Pictures/apod/'
 url = 'https://api.nasa.gov/planetary/apod?api_key=zaVObY9zGhMh20jhIaTwqUkrgdAeftR8MltzY5ye'
-http = urllib3.PoolManager()
 
-response = http.request('GET', url)
-print("Information retrieved...")
 
-response = json.loads(response.data.decode('utf-8'))
+class InvalidMediaType(Exception):
+    """Raised when the media type of that day is not image"""
+    pass
 
-if response['media_type'] == 'image':
-    imgurl = response['hdurl']
-    imgname = imgurl.strip().split('/')[-1]
-    saveAs = directory + response['date'] + '_' + imgname
-    urllib.request.urlretrieve(imgurl, saveAs)
-    print("Image retrieved...")
 
-    # TODO: search for a possibly better way to do this (fit in the windows options etc)
-    os.system('gsettings set org.gnome.desktop.background picture-uri file://' + saveAs)
-    notification = ['notify-send', response['explanation']]
-    sp.run(notification)
+def notify(message):
+    # TODO: Not the notification type I wanted. Do research
+    p = Popen(['notify-send', message])
+    p.wait()
 
-else:
-    os.system('notify-send \'No new picture for today :( \'')
+
+def setWallpaper(saveAs):
+    args = ['gsettings', 'set', 'org.gnome.desktop.background', 'picture-uri', 'file://' + saveAs]
+    q = Popen(args)
+    q.wait()
+    print('Wallpaper set.')
+
+
+def getInfo(url):
+    http = urllib3.PoolManager()
+    response = http.request('GET', url)
+    print("Information retrieved...")
+    return json.loads(response.data.decode('utf-8'))
+
+
+def downloadImage(response):
+    if response['media_type'] == 'image':
+        imgUrl = response['hdurl']
+        imgName = imgUrl.strip().split('/')[-1]
+        saveAs = directory + response['date'] + '_' + imgName
+        urllib.request.urlretrieve(imgUrl, saveAs)
+        print("Image retrieved...")
+        return saveAs, response['explanation']
+    else:
+        raise InvalidMediaType
+
+
+def run():
+    response = getInfo(url)
+    try:
+        saveAs, explanation = downloadImage(response)
+        setWallpaper(saveAs)
+        notify(explanation)
+    except InvalidMediaType:
+        notify('Media type not supported.')
+
+
+if __name__ == '__main__':
+    run()
